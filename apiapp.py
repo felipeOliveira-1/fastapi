87511ccb1242e0c79d6428c2932962868d064321
felipeOliveira-1 <contato@felipesilva.tec>
2024-04-13 11:13:15 -0300

    commit

---
+++ apiapp.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+richest_people_list = {
+    "Elon Musk": "280 Billion USD", 
+    "Jeff Bezos": "250 Billion USD", 
+    "Bill Gates": "200 Billion USD", 
+    "Mark Zukerberg": "180 Billion USD"    
+}
+
+@app.get("/richest-people")
+def richest_people():
+    return richest_people_list
